01d2110db58d3c4819d3e4c2fd3a4a3357a37994
tran-jordan <jordantran092@gmail.com>
2025-05-26 15:58:47 -0400

    a

--- controller.js
+++ controller.js
@@ -1,15 +1,22 @@
-const navBarElement = document.querySelector(".navbar");
+
+
+
 
 // Window will track if user is scrolling, if so will call setNavBackground
-window.addEventListener(scroll, setNavBackground);
+window.addEventListener("scroll", setNavBackground);
 
-/*
-
-If scrollY has reached the height of navbar, then make navbar solid, else transparent. Then apply smooth animation in css 0.5s. 
-Might have to find method to get length of navbar height because it changes with screen size
-
-*/
 
 function setNavBackground() {
-    if(scrollY )
+    // Must define navBarElement in scope of this function or else will be initialized as undefined even though it may have been initialized outside of scope
+    const navBarElement = document.querySelector(".navbar");
+    
+    let navHeight = navBarElement.offsetHeight;
+    
+    // Adds the scrolled class to navbar, giving the solid background to navbar if scrolled past the navbar height. Else, removes the solid background
+    if(window.scrollY >= navHeight) {
+        navBarElement.classList.add("scrolled");
+    }
+    else if(window.scrollY < navHeight) {
+        navBarElement.classList.remove("scrolled");
+    }
 }
