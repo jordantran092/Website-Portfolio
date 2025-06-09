e03bfb2077d0bd39ce440196089952635bc39ee1
tran-jordan <jordantran092@gmail.com>
2025-06-09 10:53:02 -0400

    a

--- controller.js
+++ controller.js
@@ -1,5 +1,4 @@
 
-let expanded = false;
 let scrolled = false;
 
 // [ Solid background transition ]
@@ -11,35 +10,40 @@ function setNavBackground() {
     const navBarTogglerElement = document.querySelector(".navbar-toggler-icon");
     const navBarContentElement = document.getElementById("navbarSupportedContent");
     
-    let navHeight = navBarElement.offsetHeight;
+    const navHeight = navBarElement.offsetHeight;
     
 
     
-    if(!navBarContentElement.classList.contains("show")) {
+    
         
-        // Adds the scrolled class to navbar, giving the solid background to navbar if scrolled past the navbar height. Else, removes the solid background
-        if(window.scrollY >= navHeight) {
+    // Adds the scrolled class to navbar, giving the solid background to navbar if scrolled past the navbar height. Else, removes the solid background
+    if(window.scrollY >= navHeight) {
+        if(!navBarContentElement.classList.contains("show")) {
             navBarElement.classList.add("scrolled");
             
             // Changes navbar toggler to black
             navBarTogglerElement.style.backgroundImage = `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'%3e%3cpath stroke='rgba%280, 0, 0, 1%29' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e")`;
-
-            scrolled = !scrolled;
-
-            
         }
-        else if(window.scrollY < navHeight) {
+
+
+        scrolled = true;
+        
+    }
+    else if(window.scrollY < navHeight) {
+        if(!navBarContentElement.classList.contains("show")) {
             navBarElement.classList.remove("scrolled");
 
             // Changes navbar toggler back to white, only for home page, for easier visibility
             if (window.location.pathname === '/index.html') {
                 navBarTogglerElement.style.backgroundImage = `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'%3e%3cpath stroke='rgba%28255, 255, 255, 1%29' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e")`;
             }
-
-            scrolled = !scrolled;
-
         }
+
+        scrolled = false;
+        
+
     }
+    
 }
 
 
@@ -47,13 +51,25 @@ function setNavBackground() {
 
 function setNavBackgroundOnClick() {
     const navBarElement = document.querySelector(".navbar");
+    
+
+    if(!scrolled) {
+        
+        if(window.getComputedStyle(navBarElement).backgroundColor == "rgb(255, 255, 255)") {
+            navBarElement.classList.remove("scrolled");
+        }
+        else {
+            navBarElement.classList.add("scrolled");
+        }
+    }
+
 
     // if(!scrolled && expanded == false) {
     //     navBarElement.classList.add("scrolled");
     //     expanded = !expanded;
     // }
     // else if(!scrolled && expanded) {
-    //     navBarElement.classList.remove("scrolled");
+    
     //     expanded = !expanded;
     // }
 
